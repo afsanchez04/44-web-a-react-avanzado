061a76e0a2541eab37afadac6905026cb9928e99
afsaanchez04 <afsanchez04@gmail.com>
2026-04-20 21:14:33 -0500

    useOllama.jsx y ChatContext.jsx actualizados

--- src/hooks/useOllama.jsx
+++ src/hooks/useOllama.jsx
@@ -1,29 +1,10 @@
 import axios from 'axios'
-import { useReducer, useState } from 'react'
-
-const initialState = {
-  messages: []
-}
-
-const chatReducer = (state, action) => {
-  switch (action.type) {
-    case 'ADD_MESSAGE':
-      console.log('Agregando mensaje...')
-      console.log(state)
-      return { ...state, messages: [...state.messages, action.payload] }
-
-    default:
-      return state
-  }
-}
+import { useState } from 'react'
 
 export const useOllama = () => {
   const [loading, setLoading] = useState(false)
-  const [state, dispatch] = useReducer(chatReducer, initialState)
 
   const sendMessage = async (userPrompt) => {
-    dispatch({ type: 'ADD_MESSAGE', payload: { from: 'user', text: userPrompt } })
-
     try {
       const res = await axios.post('http://localhost:11434/api/generate', {
         model: 'gemma2',
@@ -31,7 +12,6 @@ export const useOllama = () => {
         stream: false
       })
 
-      dispatch({ type: 'ADD_MESSAGE', payload: { from: 'bot', text: res.data.response } })
       return res
     } catch (error) {
       console.error('Error:', error)
