5b739d227681619bf32cfef523296ae959a27b48
afsaanchez04 <afsanchez04@gmail.com>
2026-04-27 21:35:14 -0500

    creación de rutas y parámetros

--- express/src/app.js
+++ express/src/app.js
@@ -8,11 +8,81 @@
 
 import { config } from 'dotenv'
 import express from 'express'
+import { infoPeliculas } from './peliculas.js'
 config() 
 
 const app = express()
 
+app.get("/", (req, res) => {
+  res.send("Servidor de express corriendo...")
+})
 
-//Variables de entorno
-console.log(process.env.PORT)
-console.log(process.env.HELLO)
+app.get("/api/peliculas", (req, res) => {
+  res.send(infoPeliculas)
+})
+
+/* Acción */
+
+app.get("/api/peliculas/accion", (req, res) => {
+  res.send( infoPeliculas.accion )
+})
+
+app.get("/api/peliculas/accion/:titulo", (req, res) => {
+
+  const titulo = req.params.titulo
+  const resultados = infoPeliculas.accion.filter( pelicula => pelicula.titulo === titulo )
+
+  if( resultados.length === 0 ){
+    return res.status(404).send(`No se encontraron resultados para ${titulo}`)
+  }
+
+  res.send( resultados )
+})
+
+app.get("/api/peliculas/accion/year/:year", (req, res) => {
+
+  const year = Number (req.params.year)
+
+  const resultados = infoPeliculas.accion.filter( pelicula => pelicula.year === year )
+
+  if( resultados.length === 0 ){
+    return res.status(404).send(`No se encontraron resultados de ${year}`)
+  }
+
+  res.send(resultados)
+
+})
+
+app.get("/api/peliculas/accion/titulo/:titulo/:year", (req, res) => {
+  const {titulo, year} = req.params
+
+  const resultados = infoPeliculas.accion.filter( pelicula => pelicula.titulo === titulo && pelicula.year === Number(year) )
+
+  if(resultados.length === 0){
+    return res.status(404).send(`No se encontraron resultados para ${titulo} del ${year}`)
+  }
+
+  res.send(resultados)
+
+})
+
+/* Drama */
+
+app.get("/api/peliculas/drama", (req, res) => {
+  res.send( infoPeliculas.drama )
+})
+
+app.get("/api/peliculas/drama/:titulo", (req, res) => {
+    const titulo = req.params.titulo
+    const resultados = infoPeliculas.drama.filter( pelicula => pelicula.titulo === titulo )
+    if (resultados.length === 0) {
+        return res.status(404).send(`No se encontraron resultados para ${titulo}`)
+}
+    res.send(resultados)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen( PORT, () => {
+  console.log(`El servidor está escuchando el puerto http://localhost:${PORT}`)
+} )
