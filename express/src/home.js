d2caa4582206e2c2348ec8aa1c9865ae03b81c27
afsaanchez04 <afsanchez04@gmail.com>
2026-04-29 22:35:32 -0500

    funciones readData y writeData creados

--- express/src/home.js
+++ express/src/home.js
@@ -1,13 +1,39 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import fs from 'fs'
 
 dotenv.config()
 const app = express()
 
 const PORT = process.env.PORT
 
+
+//Función que lee la información de peliculas.json
+const readData = () => {
+  try{
+    const data = fs.readFileSync('./src/peliculas.json')
+    return JSON.parse( data )
+  }catch(error){
+    console.error(error)
+  }
+} 
+
+//Función que escribe dentro del archivo de peliculas.json
+const writeData = (data) => {
+  try {
+    fs.writeFileSync('./src/peliculas.json', JSON.stringify(data) )
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 app.get("/", (req, res) => {
   res.send("Hola mundo server corriendo")
+})
+
+app.get( "/peliculas", (req, res)  => {
+  const data = readData()
+  res.json(data)
 })
 
 
